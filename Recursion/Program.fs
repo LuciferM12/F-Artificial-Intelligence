efa8ad220e84ec1bfbed732dd5a4c3cbe7d4260c
LuciferM121 <rojasrodriguezmartinomar@gmail.com>
2025-02-03 17:23:49 -0600

    feat: add recursion examples

---
+++ Recursion/Program.fs
@@ -0,0 +1,24 @@
+﻿//Rec example
+let rec append xs ys =
+    match xs with
+    | [] -> ys
+    | x :: xs -> x :: append xs ys
+
+let rec sum = function 
+    | [] -> 0
+    | x :: xs -> x + sum xs
+
+let rec sep z xs =
+    match xs with
+    | [] -> []
+    | [_] -> xs
+    | x :: xs -> x :: z :: sep z xs
+
+printf "sep: %A\n" (sep -1 [2])
+
+let rec reverse xs =
+    match xs with
+    | [] -> []
+    | x :: xs -> reverse xs @ [x]
+
+printf "reverse: %A" (reverse [1;2;3])
